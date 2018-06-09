5c3e8e35c1259255db549924e4da3da42dd5945f
Abdul Aziz Tetteh <abdulaziztetteh@gmail.com>
2018-06-09 16:11:23 +0100

    add mvc controller

--- src/main/java/a4bdul/springframework/springbootjokesapp/controllers/JokesController.java
+++ src/main/java/a4bdul/springframework/springbootjokesapp/controllers/JokesController.java
@@ -1,7 +1,28 @@
 package a4bdul.springframework.springbootjokesapp.controllers;
 
+import a4bdul.springframework.springbootjokesapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by 4bdul on 09/06/2018 at 3:56 PM.
  */
+@Controller
 public class JokesController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokesController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke",jokeService.getJoke());
+
+        return "chucknorris";
+    }
 }
--- src/main/java/a4bdul/springframework/springbootjokesapp/models/Joke.java
+++
@@ -1,7 +0,0 @@
-package a4bdul.springframework.springbootjokesapp.models;
-
-/**
- * Created by 4bdul on 09/06/2018 at 3:56 PM.
- */
-public class Joke {
-}
