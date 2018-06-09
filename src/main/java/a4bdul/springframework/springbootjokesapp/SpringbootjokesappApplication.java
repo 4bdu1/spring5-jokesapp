df166e68aa67bd4342ff839ca60a6f07dd9f3c21
Abdul Aziz Tetteh <abdulaziztetteh@gmail.com>
2018-06-09 17:00:48 +0100

    add xml configuration

--- src/main/java/a4bdul/springframework/springbootjokesapp/SpringbootjokesappApplication.java
+++ src/main/java/a4bdul/springframework/springbootjokesapp/SpringbootjokesappApplication.java
@@ -2,8 +2,10 @@ package a4bdul.springframework.springbootjokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class SpringbootjokesappApplication {
 
 	public static void main(String[] args) {
--- src/main/java/a4bdul/springframework/springbootjokesapp/config/ChuckConfiguration.java
+++ src/main/java/a4bdul/springframework/springbootjokesapp/config/ChuckConfiguration.java
@@ -7,10 +7,10 @@ import org.springframework.context.annotation.Configuration;
 /**
  * Created by 4bdul on 09/06/2018 at 4:50 PM.
  */
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
-    @Bean
+//    @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
