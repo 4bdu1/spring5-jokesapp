663db91919fa805e95960d5f4cf4bc7be7ef301e
Abdul Aziz Tetteh <abdulaziztetteh@gmail.com>
2018-06-09 16:53:44 +0100

    add java configuration

---
+++ src/main/java/a4bdul/springframework/springbootjokesapp/config/ChuckConfiguration.java
@@ -0,0 +1,17 @@
+package a4bdul.springframework.springbootjokesapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by 4bdul on 09/06/2018 at 4:50 PM.
+ */
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+}
